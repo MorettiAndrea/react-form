0ceb8851ea88ede050c7b440224fe4eff51cb1be
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-05-27 16:59:36 +0200

    pausa

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,47 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 export default function App() {
   const [articles, setArticles] = useState([
     { id: 1, giornale: "corriere dello sport", title: "Serie A" },
     { id: 2, giornale: "il fatto quotidiano", title: "Politica" },
-    { id: 3, giornale: "focus", title: "Scienza" },
+    { id: 3, giornale: "focus", title: "Lo spazio" },
   ]);
+  const [newArticles, setNewArticles] = useState("");
 
-  // gestione cambio input
+  // gestione aggiunta elemento
 
-  const hadleinputChange = (e) => {
+  const handleNewArticle = (e) => {
     e.preventDefault();
-    const newArticles = { ...articles, [e.target.name]: e.target.title };
-    setArticles(newArticles);
-    console.log(newArticles);
+    const newArticlesList = { ...articles, newArticles };
+    setArticles(newArticlesList);
+    setNewArticles("");
+  };
+  // gestione cancellazione
+  const handleArticleDelete = (idToDelete) => {
+    const updatedArticles = articles.filter((article) => article.id !== idToDelete);
+    setArticles(updatedArticles);
   };
 
   // struttura
 
   return (
     <main className="custom-container d-flex">
-      <form>
-        <div className="form-row">
-          <input type="text" name="Serie A" title={articles.title} onChange={hadleinputChange} />
-        </div>
-        <div className="form-row">
-          <input type="text" name="Politica" title={articles.title} onChange={hadleinputChange} />
-        </div>
-        <div className="form-row">
-          <input type="text" name="Scienza" title={articles.title} onChange={hadleinputChange} />
-        </div>
-      </form>
+      <div className="form-row">
+        <form onSubmit={handleNewArticle}>
+          <input type="text" value={newArticles} onChange={(e) => setNewArticles(e.target.value)} />
+        </form>
+      </div>
+      <div>
+        <h2>Articoli</h2>
+        <ul>
+          {articles.map((article) => (
+            <li key={article.id}>
+              {article.title}
+              <button onClick={() => handleArticleDelete(article.id)}>cancella;</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>
   );
 }
